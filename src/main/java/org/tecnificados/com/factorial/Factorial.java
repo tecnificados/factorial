afebebaafa0255bcbc317bf7958d601c8f4c1ce7
Juan Carlos Ballesteros <juancarlos.ballesteros@auragroup.es>
2021-11-21 16:37:44 +0100

    Factorial

---
+++ src/main/java/org/tecnificados/com/factorial/Factorial.java
@@ -0,0 +1,68 @@
+package org.tecnificados.com.factorial;
+
+import java.math.BigInteger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+* Factorial used to generate a factorial of an integer.
+*
+* @author Juan Carlos Ballesteros Hermida - Tecnificados
+*/
+public class Factorial {
+	
+	private static final Logger log = LoggerFactory.getLogger(Factorial.class);
+
+	public static void main(String[] args) {
+		BigInteger numberToGenerateFactorial;
+		BigInteger factorial;
+
+		if (args.length != 1) {
+			log.error("We need only one argument to generate the factorial. Example: 6");
+			return;
+		}
+
+		try {
+			numberToGenerateFactorial = new BigInteger(args[0]);
+		} catch (Exception e) {
+			log.error("We need a integer greater than 0 to generate the factorial.");
+			return;
+		}
+
+		if (numberToGenerateFactorial.signum()<0) {
+			log.error("We need a positive integer to generate the factorial.");
+			return;
+		}		
+		factorial = calculateFactorial(new BigInteger(numberToGenerateFactorial + ""));
+		if (factorial!=null)
+		{
+			log.info("The factorial of "+numberToGenerateFactorial+" is: "+factorial.toString());
+		}
+	}
+
+	public static BigInteger calculateFactorial(BigInteger integerF) {
+
+		try
+		{
+			if (integerF==null)
+				return integerF;
+			else if (integerF.equals(BigInteger.ZERO))
+				return BigInteger.ONE;
+			else if (integerF.signum()<0)
+				return null;
+			else
+			{
+				BigInteger calc = calculateFactorial(integerF.subtract(BigInteger.ONE));
+				if (calc==null)
+				{
+					return null;
+				}
+				return integerF.multiply(calc);
+			}
+		}
+		catch(StackOverflowError | Exception exc){
+            log.error("The number is too big. Error at "+integerF.toString());
+            return null;
+        }		
+	}
+}
---
+++ src/test/java/tecnificados/com/factorial/FactorialTest.java
@@ -0,0 +1,57 @@
+package tecnificados.com.factorial;
+
+import java.math.BigInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tecnificados.com.factorial.Factorial;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class FactorialTest extends TestCase {
+	
+	private static final Logger log = LoggerFactory.getLogger(FactorialTest.class);
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public FactorialTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(FactorialTest.class);
+	}
+
+	public void testFactorialZero() {
+		log.info("testFactorialZero");
+		BigInteger calculateFactorial = Factorial.calculateFactorial(BigInteger.ZERO);
+		assertTrue("Check if factorial of 0 is 1", calculateFactorial.toString().equals("1"));
+	}
+	
+	public void testFactorialNegative() {
+		log.info("testFactorialNegative");
+		BigInteger calculateFactorial = Factorial.calculateFactorial(new BigInteger("-1"));
+		assertTrue("Check if factorial of a negative integer is null", calculateFactorial==null);
+	}
+	
+	public void testFactorial5() {
+		log.info("testFactorial5");
+		BigInteger calculateFactorial = Factorial.calculateFactorial(new BigInteger("5"));
+		log.info("\t"+calculateFactorial.toString());
+		assertTrue("Check if factorial of 5 is 120", calculateFactorial.toString().equals("120"));
+	}
+	
+	public void testFactorialBig() {
+		log.info("testFactorial5");
+		BigInteger calculateFactorial = Factorial.calculateFactorial(new BigInteger("999999999"));
+		log.info("\tIs null: "+(calculateFactorial==null));
+		assertTrue("Check if factorial of a big number is null", calculateFactorial==null);
+	}
+}
